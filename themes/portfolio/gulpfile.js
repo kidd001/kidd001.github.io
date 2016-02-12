ac50923bc470ed9fda15778fb8248a3ecacd17f7
Kexin Li <kidd001@hotmail.com>
2016-02-12 16:14:34 +1300

    gulp sass WIP

--- themes/portfolio/gulpfile.js
+++ themes/portfolio/gulpfile.js
@@ -3,16 +3,19 @@ var gulp = require('gulp'),
 	uglify = require('gulp-uglify'),
 	concat = require('gulp-concat'),
 	minifyCSS = require('gulp-minify-css');
+  sass = require('gulp-sass');
+  sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('compress', function() {
   gulp.src(['assets/vendor/jquery/dist/jquery.js', 'assets/vendor/lightslider/dist/js/lightslider.js', 'assets/js/scripts.js'])
     .pipe(concat('all.min.js'))
     .pipe(uglify())
     .pipe(gulp.dest('static/js/'));
-  gulp.src(['assets/css/poole.css', 'assets/css/hyde.css', 'assets/css/syntax.css'])
-    .pipe(concat('style.min.css'))
-    .pipe(minifyCSS())
-    .pipe(gulp.dest('static/css/'));
+  gulp.src('assets/scss/base.scss')
+    .pipe(sourcemaps.init())  // Process the original sources
+      .pipe(sass())
+    .pipe(sourcemaps.write()) // Add the map to modified source.
+    .pipe(gulp.dest('static/css/'));  
 });
 
 gulp.task('watch', function() {
